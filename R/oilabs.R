f247414776b09aeef5e28c639e1d3f7dc782145a
AmeliaMN <amelia.a.mcnamara@gmail.com>
2015-09-18 15:03:00 -0400

    overall package documentation

---
+++ R/oilabs.R
@@ -0,0 +1,6 @@
+#' oilabs: An R package accompanying the OpenIntro labs.
+#' 
+#' The oilabs package contains the data sets, custom functions, documentation, and templates for the OpenIntro Labs.
+#' 
+#'  @docType package
+#'  @name oilabs
