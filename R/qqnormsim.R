532d3fce547698005ef05972ef477017e0eac8a2
andrewpbray <andrew.bray@gmail.com>
2015-09-29 08:24:50 -0700

    Fiddle with spacing.

--- R/qqnormsim.R
+++ R/qqnormsim.R
@@ -1,9 +1,9 @@
 #' Generate simulated QQ plots
 #'
 #' Create a 3 x 3 grid of quantile-quantile plot, the first of which corresponds
-#' to the input data. The other 8 plots arise from simulating random normal data
-#' with the same mean, standard deviation, and length as the data. For use in
-#' comparing known-normal qqplots to an observed qqplot to assess normality.
+#' to the input data. The other eight plots arise from simulating random normal
+#' data with the same mean, standard deviation, and length as the data. For use
+#' in comparing known-normal qqplots to an observed qqplot to assess normality.
 #'
 #' @param sample the variable to be plotted.
 #' @param data data frame to use.
