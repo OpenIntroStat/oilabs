0f67743dd9ca2f7e2ebd946b736738a14e8c4a08
andrewpbray <andrew.bray@gmail.com>
2015-10-01 11:29:10 -0700

    Typo.

--- R/qqnormsim.R
+++ R/qqnormsim.R
@@ -1,6 +1,6 @@
 #' Generate simulated QQ plots
 #'
-#' Create a 3 x 3 grid of quantile-quantile plot, the first of which corresponds
+#' Create a 3 x 3 grid of quantile-quantile plots, the first of which corresponds
 #' to the input data. The other eight plots arise from simulating random normal
 #' data with the same mean, standard deviation, and length as the data. For use
 #' in comparing known-normal qqplots to an observed qqplot to assess normality.
