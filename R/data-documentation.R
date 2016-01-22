5b149d10a0ef69c6dd716aad2bb71c1c1934dca3
mine-cetinkaya-rundel <mine@stat.duke.edu>
2016-01-21 23:27:44 -0500

    documentation update + present data update to 2013

--- R/data-documentation.R
+++ R/data-documentation.R
@@ -22,17 +22,20 @@
 #' Male and female births in the US
 #'
 #' Counts of the total number of male and femals births in the United States from
-#' 1940 to 2002.
+#' 1940 to 2013.
 #'
-#' @format A data frame with 63 rows and 3 variables:
+#' @format A data frame with 74 rows and 3 variables:
 #' \describe{
-#'   \item{year}{year, ranging from 1940 to 2002}
+#'   \item{year}{year, ranging from 1940 to 2013}
 #'   \item{boys}{number of male births}
 #'   \item{girls}{number of female births}
 #' }
-#' @source These data appear in Mathews TJ, and Hamilton BE. 2005. Trend
+#' @source Data up to 2002 appear in Mathews TJ, and Hamilton BE. 2005. Trend
 #' analysis of the sex ratio at birth in the United States. National Vital
-#' Statistics Reports 53(20):1-17.
+#' Statistics Reports 53(20):1-17. Data for 2003 - 20013 have been collected
+#' from annual National Vital Statistics Reports published by the US Department of 
+#' Health and Human Services, Centers for Disease Control and Prevention, 
+#' National Center for Health Statistics.
 "present"
 
 #' Behavioral survey
