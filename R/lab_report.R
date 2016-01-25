5ac3c8a4fe12739a27579fd2e38efb33afe63512
ismayc <chester.ismay@gmail.com>
2016-01-24 17:24:20 -0800

    Exported lab_report function

--- R/lab_report.R
+++ R/lab_report.R
@@ -2,8 +2,7 @@
 #' 
 #' @export
 
-lab_report <- function(highlight = "pygments",
-  theme = "spacelab"){
+lab_report <- function(highlight = "pygments", theme = "spacelab"){
   if(!require(rmarkdown))
     install.packages("rmarkdown", repos = "http://cran.rstudio.com")
   rmarkdown::html_document(highlight = highlight,
